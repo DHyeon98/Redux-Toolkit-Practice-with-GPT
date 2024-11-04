4d343747586ffe6fe0849d149d832eefe4e803df
LimDong <ilhai6@naver.com>
2024-11-04 22:04:16 +0900

    slice 생성

---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterType {
+  value: number;
+}
+
+const initialState: CounterType = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter", // Slice 이름
+  initialState,
+  reducers: {
+    // 상태를 변경하는 함수들을 정의하는 객체
+    // state는 현재 상태를 참조하며, Redux Toolkit을 사용하면 state를 직접 수정해도 불변성이 자동으로 보장되도록 처리됩니다.
+    add: (state) => {
+      state.value += 1;
+    },
+    minus: (state) => {
+      state.value -= 1;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+  },
+});
+
+export const { add, minus, reset } = counterSlice.actions;
+export default counterSlice.reducer;
